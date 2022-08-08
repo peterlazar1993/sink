2ffa954bb1535fb6a5a0949041f2e704794425ce
Peter Lazar <peterlazar1993@gmail.com>
2022-08-09 00:14:25 +0530

    chore: add spinner with reanimated

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
-import { Box } from './src/Animation';
+import { Box } from './src/reanimated/Spinner';
 
 export default function App() {
   return (
@@ -15,5 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
