f9af0ae8314333ad436976787b9570ad966fb685
Peter Lazar <peterlazar1993@gmail.com>
2022-08-23 20:17:12 +0530

    chore: add safe area provider

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import { InterpolateColors } from './src/reanimated/InterpolateColors';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <InterpolateColors />
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <View style={styles.container}>
+            <InterpolateColors />
+            <StatusBar style="auto" />
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
